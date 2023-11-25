86817bb0adabdd1cd527e39c7f2cf6143925a98f
Ekaterina <shi.e.m@yandex.ru>
2023-11-25 13:50:06 +0300

    TestNG

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,39 @@
+package ru.netology;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void whenPurchaseLessBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void whenPurchaseEqualBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void whenPurchaseMoreBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1300;
+
+        int actual = service.remain(amount);
+        int expected = 700;
+
+        Assert.assertEquals(actual, expected);
+    }
+}
